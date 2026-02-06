ccbb23833bee9c19cdf40dc5dabfda60996eb166
hoangchitrung <trunghoang1124@gmail.com>
2026-02-06 16:08:11 +0700

    test for the presentation

--- test_saucedemo.py
+++ test_saucedemo.py
@@ -1,12 +1,8 @@
 import re
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Page, expect
 
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
-    context = browser.new_context()
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    page = context.new_page()
+def test_happy_case(page: Page) -> None:
     page.goto("https://www.saucedemo.com/")
     page.locator('[data-test="username"]').click()
     page.locator('[data-test="username"]').fill("standard_user")
@@ -17,19 +13,42 @@ def run(playwright: Playwright) -> None:
     page.locator('[data-test="shopping-cart-link"]').click()
     page.locator('[data-test="checkout"]').click()
     page.locator('[data-test="firstName"]').click()
-    page.locator('[data-test="firstName"]').fill("test")
+    page.locator('[data-test="firstName"]').fill("Hong")
     page.locator('[data-test="lastName"]').click()
-    page.locator('[data-test="lastName"]').fill("hehe")
+    page.locator('[data-test="lastName"]').fill("Trung")
     page.locator('[data-test="postalCode"]').click()
-    page.locator('[data-test="postalCode"]').fill("123456")
+    page.locator('[data-test="postalCode"]').fill("00700")
     page.locator('[data-test="continue"]').click()
     page.locator('[data-test="finish"]').click()
-    print("✅ Test Pass: Đã xác nhận hoàn tất đơn hàng!")
-    context.tracing.stop(path="trace.zip")
-    # ---------------------
-    context.close()
-    browser.close()
+    page.locator('[data-test="back-to-products"]').click()
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+def test_negative_case(page: Page) -> None:
+    page.goto("https://www.saucedemo.com/")
+    page.locator('[data-test="username"]').click()
+    page.locator('[data-test="username"]').fill("locked_out_user")
+    page.locator('[data-test="password"]').click()
+    page.locator('[data-test="password"]').fill("secret_sauce")
+    page.locator('[data-test="login-button"]').click()
+
+
+def test_performance_case(page: Page) -> None:
+    page.goto("https://www.saucedemo.com/")
+    page.locator('[data-test="username"]').click()
+    page.locator('[data-test="username"]').fill("performance_glitch_user")
+    page.locator('[data-test="password"]').click()
+    page.locator('[data-test="password"]').fill("secret_sauce")
+    page.locator('[data-test="login-button"]').click()
+    page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    page.locator('[data-test="shopping-cart-link"]').click()
+    page.locator('[data-test="checkout"]').click()
+    page.locator('[data-test="firstName"]').click()
+    page.locator('[data-test="firstName"]').fill("TEst")
+    page.locator('[data-test="firstName"]').press("Tab")
+    page.locator('[data-test="lastName"]').fill("Aha")
+    page.locator('[data-test="postalCode"]').click()
+    page.locator('[data-test="postalCode"]').fill("00700")
+    page.locator('[data-test="continue"]').click()
+    page.locator('[data-test="finish"]').click()
+    page.locator('[data-test="back-to-products"]').click()
+    page.goto("https://www.saucedemo.com/inventory-item.html?id=5")
